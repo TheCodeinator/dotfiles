2452da8a4a64c8e26fb167be176867280924eae9
Luis Wientgens <windgans@protonmail.com>
2024-11-18 19:31:37 +0100

    Add wezterm and swaync

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require 'wezterm'
+local config = {}
+
+config.font = wezterm.font 'JetBrains Mono'
+config.color_scheme = 'Catppuccin Mocha'
+config.window_background_opacity = 0.8
+
+return config 
